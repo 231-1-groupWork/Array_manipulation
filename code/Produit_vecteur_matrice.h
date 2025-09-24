7f004f5d2f94437d09f13c230b26cf9db9f7aa39
kamgatafone <kamgatafone@gmail.com>
2025-09-24 23:49:29 +0100

    creation of the matrix multiplication feature to as a branchj

--- code/Produit_vecteur_matrice.h
+++ code/Produit_vecteur_matrice.h
@@ -1,5 +1,16 @@
 #ifndef __Produit_vecteur_matrice__H__
 #define __Produit_vecteur_matrice__H__
 
+//matrix definition
+struct matrix {
+    int row_size;
+    int cols_size;
+    float **m;
+}matrix;
+
+// fonction to multiply a matrix by a vector
+// let A as our matrix and V as our vector
+// precondition: A cols_size should egals to V row_size giving asset()
+(float**)matrix_multiply(matrix*A,matrix*v);
 
 #endif __Produit_vecteur_matrice__H__
